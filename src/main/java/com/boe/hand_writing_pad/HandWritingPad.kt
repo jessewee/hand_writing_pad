19e4d2febe16923ceb54aae6480da0bdc706d78c
weishujie <jessewee@hotmail.com>
2021-03-11 15:27:22 +0800

    [tmp]

--- src/main/java/com/boe/hand_writing_pad/HandWritingPad.kt
+++ src/main/java/com/boe/hand_writing_pad/HandWritingPad.kt
@@ -1,25 +1,49 @@
 package com.boe.hand_writing_pad
 
+import android.content.Context
 import android.graphics.*
+import android.util.Log
 import android.view.MotionEvent
+import java.lang.ref.WeakReference
 
 /** 手写板，主要是在画布上绘制。[onChange] 绘制内容改变，主要用来在外部控制刷新显示 */
-class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> Unit) {
+class HandWritingPad(
+    width: Int,
+    height: Int,
+    private val applicationContext: Context,
+    private val onChange: (Bitmap) -> Unit
+) {
 
     // 绘制的图片，当作参数传入画布，所有绘制结果都在这个图片上
     private var bitmap: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
 
     // 用来绘制的画布
-    private val canvas: Canvas = Canvas(bitmap).apply {
-        drawColor(Color.WHITE)
-        onChange(bitmap)
-    }
-
-    // 对象池，复用对象，避免在draw方法中重复创建对象，防止内存抖动
-    private val objectPool = mutableMapOf<Class<*>, Any>()
+    private val canvas: Canvas = Canvas(bitmap)
 
     // 操作步骤
     private val steps = mutableListOf<Step>()
+
+    // 操作栏
+    private val operationBar = OperationBar()
+
+    // 光标类型
+    private var cursorType: CursorType = CursorType.PENCIL
+
+    // 画笔
+    private val paint = Paint().apply {
+        strokeJoin = Paint.Join.ROUND
+        color = Color.BLACK
+    }
+
+    // 路径，绘制时统一使用这一个对象，防止创建过多的对象，每次绘制前reset
+    private val path = Path()
+
+    // 矩形，绘制时统一使用这一个对象，防止创建过多的对象，每次绘制前set
+    private val rect = Rect()
+
+    init {
+        draw()
+    }
 
     /** 手写板宽高改变，改变后步骤清空，不能再回退 */
     fun changeLayout(width: Int, height: Int) {
@@ -32,7 +56,6 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
         val newRatio = width.toFloat() / height.toFloat()
         val oldBitmap = bitmap
         // 计算旧底图在新底图的绘制位置
-        val rect = getObject<Rect>()
         when {
             // 宽高比一样
             oldRatio == newRatio -> {
@@ -53,7 +76,7 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
         bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         canvas.setBitmap(bitmap)
         canvas.drawColor(Color.WHITE)
-        canvas.drawBitmap(oldBitmap, null, rect, getObject())
+        canvas.drawBitmap(oldBitmap, null, rect, paint)
         // 回收旧图片对象
         oldBitmap.recycle()
         // 清空步骤
@@ -65,6 +88,7 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
     /** touch事件 */
     fun onTouch(event: MotionEvent): Boolean {
         // 点击到操作按钮上
+        if (operationBar.onTouch(event)) return true
         // 绘制
         when (event.action) {
             // 按下
@@ -72,10 +96,9 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
                 val x = event.x
                 val y = event.y
                 val action = {
-                    val path = getObject<Path>()
                     path.reset()
                     path.moveTo(x, y)
-                    getObject<Paint>().run {
+                    paint.run {
                         style = Paint.Style.STROKE
                         strokeWidth = 5F
                     }
@@ -86,14 +109,15 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
             // 移动
             MotionEvent.ACTION_MOVE -> {
                 if (steps.isEmpty()) return false
+                val lastStep = steps.last()
+                if (lastStep.finished) return false
                 val x = event.x
                 val y = event.y
                 val action = {
-                    val path = getObject<Path>()
                     path.lineTo(x, y)
-                    canvas.drawPath(path, getObject())
+                    canvas.drawPath(path, paint)
                 }
-                steps.last().run {
+                lastStep.run {
                     actions.add(action)
                     frame.calculateStepFrame(x.toInt(), y.toInt())
                 }
@@ -104,17 +128,20 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
                 val x = event.x
                 val y = event.y
                 val action = {
-                    val path = getObject<Path>()
                     path.lineTo(x, y)
-                    canvas.drawPath(path, getObject())
+                    canvas.drawPath(path, paint)
                 }
                 steps.last().run {
                     actions.add(action)
                     frame.calculateStepFrame(x.toInt(), y.toInt())
+                    finished = true
                 }
             }
             // 取消
             MotionEvent.ACTION_CANCEL -> {
+                if (steps.isNotEmpty()) {
+                    steps.last().run { if (!finished) finished = true }
+                }
                 return false
             }
             // 其他
@@ -128,17 +155,13 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
     fun release() {
         bitmap.recycle()
         steps.clear()
-        objectPool.clear()
     }
 
     // 绘制
     private fun draw() {
-        if (steps.isEmpty()) return
         canvas.drawColor(Color.WHITE)
-        // 绘制操作栏
-        drawMenus()
         // 绘制各步骤
-        for (step in steps) {
+        if (steps.isNotEmpty()) for (step in steps) {
             step.actions.forEach { it.invoke() }
 //            // 画框，测试用，要删掉
 //            var oldColor: Int
@@ -151,37 +174,10 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
 //            canvas.drawRect(step.frame, paint)
 //            paint.color = oldColor
         }
+        // 绘制操作栏
+        operationBar.drawMenus()
         // 通知外层，绘制内容改变
         onChange(bitmap)
-    }
-
-    // 绘制操作栏
-    private fun drawMenus() {
-        // TODO
-    }
-
-    // 从对象池中获取对象
-    private inline fun <reified T> getObject(): T {
-        var obj = objectPool[T::class.java]
-        if (obj != null) return obj as T
-        // 各类型的对象创建也放到这里了
-        obj = when (T::class.java) {
-            // 画笔
-            Paint::class.java -> Paint().apply {
-                strokeJoin = Paint.Join.ROUND
-                color = Color.BLACK
-            }
-            // 路径
-            Path::class.java -> Path()
-            // 矩形
-            Rect::class.java -> Rect()
-            // float类型的矩形
-            RectF::class.java -> RectF()
-            // 类型尚未添加
-            else -> throw IllegalArgumentException("要创建的对象类型不存在")
-        }
-        objectPool[T::class.java] = obj
-        return obj as T
     }
 
     // 计算步骤里的action执行后此步骤所在区域的frame值
@@ -193,5 +189,208 @@ class HandWritingPad(width: Int, height: Int, private val onChange: (Bitmap) -> 
     }
 
     // 操作步骤
-    private data class Step(val tag: String, val frame: Rect, val actions: MutableList<() -> Unit>)
+    private data class Step(
+        // 标记
+        val tag: String,
+        // 此步骤在画布上的范围
+        val frame: Rect,
+        // 步骤的细分操作
+        val actions: MutableList<() -> Unit>,
+        // 步骤是否已经结束
+        var finished: Boolean = false
+    )
+
+    // 操作栏，目前只考虑一竖排
+    private inner class OperationBar {
+
+        // 操作栏按钮大小
+        private val menuSize = 150
+        private val menuPadding = menuSize / 4
+        private val menuRadius = menuSize / 50F
+
+        // 操作栏位置
+        var left: Int = 50
+        var top: Int = 50
+        private val right get() = left + menuSize
+        private val bottom get() = top + menuSize * menus.size
+        private val leftF get() = left.toFloat()
+        private val topF get() = top.toFloat()
+        private val rightF get() = right.toFloat()
+        private val bottomF get() = bottom.toFloat()
+
+        // 显示子菜单，值是要显示的子菜单的父级菜单，null表示不显示
+        private var showSubMenu: Menu? = null
+
+        // 按钮
+        private val menus = arrayOf(
+            // 返回按钮
+            Menu(R.drawable.ic_back) {
+                // TODO 返回
+                Log.e("=======", "返回按钮")
+            },
+            // 撤回按钮
+            Menu(R.drawable.ic_withdraw) {
+                if (steps.isNotEmpty()) {
+                    steps.removeLast()
+                    draw()
+                }
+            },
+            // 选择画笔
+            Menu(R.drawable.ic_pencil) {
+                cursorType = CursorType.PENCIL
+            },
+            // 橡皮擦
+            Menu(R.drawable.ic_eraser) {
+                // TODO
+                Log.e("=======", "橡皮擦")
+            }
+        )
+
+        /** 操作栏按钮点击 */
+        fun onTouch(event: MotionEvent): Boolean {
+            when (event.action) {
+                // 按下时判断是否点中按钮，设置按钮点中效果，调用按钮的action方法，重新绘制
+                MotionEvent.ACTION_DOWN -> {
+                    val menu = findTouchedMenu(event.x.toInt(), event.y.toInt()) ?: return false
+                    menu.pressed = true
+                    menu.action.invoke()
+                    draw()
+                    return true
+                }
+                // 抬起时判断当前是否有点中的按钮，取消点中效果，重新绘制
+                MotionEvent.ACTION_UP -> {
+                    val pressedMenus = menus.filter { it.pressed }
+                    if (pressedMenus.isEmpty())
+                        return findTouchedMenu(event.x.toInt(), event.y.toInt()) != null
+                    for (m in pressedMenus) m.pressed = false
+                    draw()
+                    return true
+                }
+                // 取消时判断当前是否有点中的按钮，取消点中效果，重新绘制
+                MotionEvent.ACTION_CANCEL -> {
+                    val pressedMenus = menus.filter { it.pressed }
+                    if (pressedMenus.isEmpty()) return false
+                    for (m in pressedMenus) m.pressed = false
+                    draw()
+                    return true
+                }
+                else -> return false
+            }
+        }
+
+        // 判断点中的按钮
+        private fun findTouchedMenu(x: Int, y: Int): Menu? {
+            if (x !in left..right || x !in top..bottom) return null
+            for ((idx, m) in menus.withIndex()) {
+                val menuTop = top + menuSize * idx
+                val menuBottom = menuTop + menuSize
+                if (y in menuTop..menuBottom) return m
+            }
+            return null
+        }
+
+        /** 绘制操作栏 */
+        fun drawMenus() {
+            val oldColor: Int
+            val oldStrokeWidth: Float
+            val oldStyle: Paint.Style
+            paint.run {
+                oldColor = paint.color
+                oldStrokeWidth = paint.strokeWidth
+                oldStyle = paint.style
+                paint.color = Color.BLACK
+                paint.strokeWidth = 2F
+                paint.style = Paint.Style.STROKE
+            }
+            canvas.drawRoundRect(leftF, topF, rightF, bottomF, menuRadius, menuRadius, paint)
+            for ((idx, m) in menus.withIndex()) drawMenuItem(m, left, top + menuSize * idx)
+            drawSubMenus()
+            paint.apply {
+                paint.color = oldColor
+                paint.strokeWidth = oldStrokeWidth
+                paint.style = oldStyle
+            }
+        }
+
+        // 绘制子菜单
+        private fun drawSubMenus() {
+            if (showSubMenu == null) return
+            // TODO
+        }
+
+        // 绘制按钮
+        private fun drawMenuItem(menu: Menu, x: Int, y: Int) {
+            val itemRight = x + menuSize
+            val itemBottom = y + menuSize
+            val resBm = menu.bitmapWr?.get() ?: vectorDrawableToBitmap(menu) ?: return
+            rect.set(
+                x + menuPadding,
+                y + menuPadding,
+                itemRight - menuPadding,
+                itemBottom - menuPadding
+            )
+            canvas.drawBitmap(resBm, null, rect, paint)
+            if (menu.pressed) {
+                val oldColor: Int
+                val oldStyle: Paint.Style
+                paint.run {
+                    oldColor = paint.color
+                    oldStyle = paint.style
+                    paint.color = Color.argb(50, 0, 0, 0)
+                    paint.style = Paint.Style.FILL
+                }
+                canvas.drawRoundRect(
+                    x.toFloat(),
+                    y.toFloat(),
+                    itemRight.toFloat(),
+                    itemBottom.toFloat(),
+                    menuRadius,
+                    menuRadius,
+                    paint
+                )
+                paint.apply {
+                    paint.color = oldColor
+                    paint.style = oldStyle
+                }
+            }
+        }
+
+        // 矢量图转Bitmap
+        private fun vectorDrawableToBitmap(menu: Menu): Bitmap? {
+            val vectorDrawable = applicationContext.getDrawable(menu.resId) ?: return null
+            return Bitmap.createBitmap(
+                vectorDrawable.intrinsicWidth,
+                vectorDrawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            ).apply {
+                val c = Canvas(this)
+                vectorDrawable.setBounds(0, 0, c.width, c.height)
+                vectorDrawable.draw(c)
+                menu.bitmapWr = WeakReference(this)
+            }
+        }
+    }
+
+    /** 按钮对象 */
+    private data class Menu(
+        val resId: Int,
+        var pressed: Boolean = false,
+        var bitmapWr: WeakReference<Bitmap>? = null,
+        val action: () -> Unit
+    )
+
+    /** 光标类型 */
+    private enum class CursorType {
+        /** 铅笔 */
+        PENCIL,
+
+        /** 橡皮擦 */
+        ERASER,
+
+        /** 橡皮擦-擦除圈起来的区域 */
+        ERASER_CIRCLE,
+
+        /** 橡皮擦-擦除某一步（某一笔画） */
+        ERASER_STEP
+    }
 }
