56a8839e5280800732eb8ef415c9e186c18f205c
weishujie <jessewee@hotmail.com>
2021-03-11 16:50:34 +0800

    [子菜单位置]

--- src/main/java/com/boe/hand_writing_pad/HandWritingPad.kt
+++ src/main/java/com/boe/hand_writing_pad/HandWritingPad.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.Log
 import android.view.MotionEvent
+import android.widget.Toast
 import java.lang.ref.WeakReference
 
 /** 手写板，主要是在画布上绘制。[onChange] 绘制内容改变，主要用来在外部控制刷新显示 */
@@ -25,6 +26,9 @@ class HandWritingPad(
 
     // 操作栏
     private val operationBar = OperationBar()
+
+    // 是否显示子菜单，一次只显示一个子菜单，所以放到外层控制
+    private var showSubMenus = false
 
     // 光标类型
     private var cursorType: CursorType = CursorType.PENCIL
@@ -125,13 +129,15 @@ class HandWritingPad(
             // 抬起
             MotionEvent.ACTION_UP -> {
                 if (steps.isEmpty()) return false
+                val lastStep = steps.last()
+                if (lastStep.finished) return false
                 val x = event.x
                 val y = event.y
                 val action = {
                     path.lineTo(x, y)
                     canvas.drawPath(path, paint)
                 }
-                steps.last().run {
+                lastStep.run {
                     actions.add(action)
                     frame.calculateStepFrame(x.toInt(), y.toInt())
                     finished = true
@@ -200,7 +206,7 @@ class HandWritingPad(
         var finished: Boolean = false
     )
 
-    // 操作栏，目前只考虑一竖排
+    /** *******************操作栏，目前只考虑一竖排******************* */
     private inner class OperationBar {
 
         // 操作栏按钮大小
@@ -218,8 +224,8 @@ class HandWritingPad(
         private val rightF get() = right.toFloat()
         private val bottomF get() = bottom.toFloat()
 
-        // 显示子菜单，值是要显示的子菜单的父级菜单，null表示不显示
-        private var showSubMenu: Menu? = null
+        // 如果按下事件没在按钮上的话，需要判断隐藏子菜单栏
+        private var touchDownHandled = false
 
         // 按钮
         private val menus = arrayOf(
@@ -240,53 +246,40 @@ class HandWritingPad(
                 cursorType = CursorType.PENCIL
             },
             // 橡皮擦
-            Menu(R.drawable.ic_eraser) {
-                // TODO
-                Log.e("=======", "橡皮擦")
-            }
+            Menu(
+                R.drawable.ic_eraser,
+                arrayOf(
+                    // TODO 图标
+                    Menu(R.drawable.ic_eraser) {
+                        cursorType = CursorType.ERASER
+                        Toast.makeText(applicationContext, "1", Toast.LENGTH_SHORT).show()
+                    },
+                    // TODO 图标
+                    Menu(R.drawable.ic_eraser) {
+                        cursorType = CursorType.ERASER_CIRCLE
+                        Toast.makeText(applicationContext, "2", Toast.LENGTH_SHORT).show()
+                    },
+                    // TODO 图标
+                    Menu(R.drawable.ic_eraser) {
+                        cursorType = CursorType.ERASER_STEP
+                        Toast.makeText(applicationContext, "3", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            )
         )
 
         /** 操作栏按钮点击 */
         fun onTouch(event: MotionEvent): Boolean {
-            when (event.action) {
-                // 按下时判断是否点中按钮，设置按钮点中效果，调用按钮的action方法，重新绘制
-                MotionEvent.ACTION_DOWN -> {
-                    val menu = findTouchedMenu(event.x.toInt(), event.y.toInt()) ?: return false
-                    menu.pressed = true
-                    menu.action.invoke()
+            if (event.action == MotionEvent.ACTION_DOWN) touchDownHandled = false
+            for (m in menus) {
+                if (m.onTouch(event)) {
                     draw()
+                    if (event.action == MotionEvent.ACTION_DOWN) touchDownHandled = true
                     return true
                 }
-                // 抬起时判断当前是否有点中的按钮，取消点中效果，重新绘制
-                MotionEvent.ACTION_UP -> {
-                    val pressedMenus = menus.filter { it.pressed }
-                    if (pressedMenus.isEmpty())
-                        return findTouchedMenu(event.x.toInt(), event.y.toInt()) != null
-                    for (m in pressedMenus) m.pressed = false
-                    draw()
-                    return true
-                }
-                // 取消时判断当前是否有点中的按钮，取消点中效果，重新绘制
-                MotionEvent.ACTION_CANCEL -> {
-                    val pressedMenus = menus.filter { it.pressed }
-                    if (pressedMenus.isEmpty()) return false
-                    for (m in pressedMenus) m.pressed = false
-                    draw()
-                    return true
-                }
-                else -> return false
             }
-        }
-
-        // 判断点中的按钮
-        private fun findTouchedMenu(x: Int, y: Int): Menu? {
-            if (x !in left..right || x !in top..bottom) return null
-            for ((idx, m) in menus.withIndex()) {
-                val menuTop = top + menuSize * idx
-                val menuBottom = menuTop + menuSize
-                if (y in menuTop..menuBottom) return m
-            }
-            return null
+            if (!touchDownHandled) showSubMenus = false
+            return false
         }
 
         /** 绘制操作栏 */
@@ -303,8 +296,7 @@ class HandWritingPad(
                 paint.style = Paint.Style.STROKE
             }
             canvas.drawRoundRect(leftF, topF, rightF, bottomF, menuRadius, menuRadius, paint)
-            for ((idx, m) in menus.withIndex()) drawMenuItem(m, left, top + menuSize * idx)
-            drawSubMenus()
+            for ((idx, m) in menus.withIndex()) m.draw(left, top + menuSize * idx)
             paint.apply {
                 paint.color = oldColor
                 paint.strokeWidth = oldStrokeWidth
@@ -312,72 +304,154 @@ class HandWritingPad(
             }
         }
 
-        // 绘制子菜单
-        private fun drawSubMenus() {
-            if (showSubMenu == null) return
-            // TODO
-        }
+        /** *******************按钮对象******************* */
+        private inner class Menu(
+            val resId: Int,
+            val subMenus: Array<Menu>? = null,
+            val action: (() -> Unit)? = null
+        ) {
+            // 按钮位置
+            private var left: Int = 0
+            private var top: Int = 0
+            private val right get() = left + menuSize
+            private val bottom get() = top + menuSize
+            private val leftF get() = left.toFloat()
+            private val topF get() = top.toFloat()
+            private val rightF get() = right.toFloat()
+            private val bottomF get() = bottom.toFloat()
 
-        // 绘制按钮
-        private fun drawMenuItem(menu: Menu, x: Int, y: Int) {
-            val itemRight = x + menuSize
-            val itemBottom = y + menuSize
-            val resBm = menu.bitmapWr?.get() ?: vectorDrawableToBitmap(menu) ?: return
-            rect.set(
-                x + menuPadding,
-                y + menuPadding,
-                itemRight - menuPadding,
-                itemBottom - menuPadding
-            )
-            canvas.drawBitmap(resBm, null, rect, paint)
-            if (menu.pressed) {
-                val oldColor: Int
-                val oldStyle: Paint.Style
-                paint.run {
-                    oldColor = paint.color
-                    oldStyle = paint.style
-                    paint.color = Color.argb(50, 0, 0, 0)
-                    paint.style = Paint.Style.FILL
+            // 子菜单是否显示中
+            private val subMenusShowing get() = showSubMenus && subMenus?.isNotEmpty() == true
+
+            // 按钮按下状态
+            private var pressed: Boolean = false
+
+            // 按钮图标
+            private var bitmapWr: WeakReference<Bitmap>? = null
+
+            /** 按钮点击事件判断 */
+            fun onTouch(event: MotionEvent): Boolean {
+                if (subMenusShowing) for (sm in subMenus!!) {
+                    if (sm.onTouch(event)) return true
                 }
+                when (event.action) {
+                    // 按下时判断是否点中按钮，设置按钮点中效果，调用按钮的点击方法，重新绘制
+                    MotionEvent.ACTION_DOWN -> {
+                        if (event.x.toInt() !in left..right || event.y.toInt() !in top..bottom) return false
+                        pressed = true
+                        return true
+                    }
+                    // 移动时判断是否移出按钮范围，取消点中效果，重新绘制
+                    MotionEvent.ACTION_MOVE -> {
+                        if (pressed && (event.x.toInt() !in left..right || event.y.toInt() !in top..bottom)) {
+                            pressed = false
+                            return true
+                        }
+                        return false
+                    }
+                    // 抬起时判断当前是否有点中的按钮，取消点中效果，重新绘制
+                    MotionEvent.ACTION_UP -> {
+                        if (pressed && event.x.toInt() in left..right && event.y.toInt() in top..bottom) {
+                            pressed = false
+                            performClick()
+                            return true
+                        }
+                        return false
+                    }
+                    // 取消时判断当前是否有点中的按钮，取消点中效果，重新绘制
+                    MotionEvent.ACTION_CANCEL -> {
+                        if (pressed) {
+                            pressed = false
+                            return true
+                        }
+                        return false
+                    }
+                    else -> return false
+                }
+            }
+
+            /** 绘制按钮 */
+            fun draw(x: Int, y: Int) {
+                left = x
+                top = y
+                val resBm = bitmapWr?.get() ?: vectorDrawableToBitmap() ?: return
+                rect.set(
+                    left + menuPadding,
+                    top + menuPadding,
+                    right - menuPadding,
+                    bottom - menuPadding
+                )
+                canvas.drawBitmap(resBm, null, rect, paint)
+                // 绘制子菜单
+                drawSubMenus()
+                // 绘制按下效果
+                if (pressed) {
+                    val oldColor: Int
+                    val oldStyle: Paint.Style
+                    paint.run {
+                        oldColor = paint.color
+                        oldStyle = paint.style
+                        paint.color = Color.argb(50, 0, 0, 0)
+                        paint.style = Paint.Style.FILL
+                    }
+                    canvas.drawRoundRect(
+                        leftF,
+                        topF,
+                        rightF,
+                        bottomF,
+                        menuRadius,
+                        menuRadius,
+                        paint
+                    )
+                    paint.apply {
+                        paint.color = oldColor
+                        paint.style = oldStyle
+                    }
+                }
+            }
+
+            // 矢量图转Bitmap
+            private fun vectorDrawableToBitmap(): Bitmap? {
+                val vectorDrawable = applicationContext.getDrawable(resId) ?: return null
+                return Bitmap.createBitmap(
+                    vectorDrawable.intrinsicWidth,
+                    vectorDrawable.intrinsicHeight,
+                    Bitmap.Config.ARGB_8888
+                ).apply {
+                    val c = Canvas(this)
+                    vectorDrawable.setBounds(0, 0, c.width, c.height)
+                    vectorDrawable.draw(c)
+                    bitmapWr = WeakReference(this)
+                }
+            }
+
+            // 按钮点击事件
+            private fun performClick() {
+                action?.invoke()
+                showSubMenus = if (subMenus?.isNotEmpty() == true) !showSubMenus
+                else false
+            }
+
+            // 绘制子菜单
+            private fun drawSubMenus() {
+                if (!subMenusShowing) return
+                val subTopF = topF + menuSize / 2
                 canvas.drawRoundRect(
-                    x.toFloat(),
-                    y.toFloat(),
-                    itemRight.toFloat(),
-                    itemBottom.toFloat(),
+                    rightF,
+                    subTopF,
+                    rightF + menuSize,
+                    subTopF + menuSize * subMenus!!.size,
                     menuRadius,
                     menuRadius,
                     paint
                 )
-                paint.apply {
-                    paint.color = oldColor
-                    paint.style = oldStyle
-                }
-            }
-        }
-
-        // 矢量图转Bitmap
-        private fun vectorDrawableToBitmap(menu: Menu): Bitmap? {
-            val vectorDrawable = applicationContext.getDrawable(menu.resId) ?: return null
-            return Bitmap.createBitmap(
-                vectorDrawable.intrinsicWidth,
-                vectorDrawable.intrinsicHeight,
-                Bitmap.Config.ARGB_8888
-            ).apply {
-                val c = Canvas(this)
-                vectorDrawable.setBounds(0, 0, c.width, c.height)
-                vectorDrawable.draw(c)
-                menu.bitmapWr = WeakReference(this)
+                for ((idx, sm) in subMenus.withIndex()) sm.draw(
+                    right,
+                    subTopF.toInt() + menuSize * idx
+                )
             }
         }
     }
-
-    /** 按钮对象 */
-    private data class Menu(
-        val resId: Int,
-        var pressed: Boolean = false,
-        var bitmapWr: WeakReference<Bitmap>? = null,
-        val action: () -> Unit
-    )
 
     /** 光标类型 */
     private enum class CursorType {
